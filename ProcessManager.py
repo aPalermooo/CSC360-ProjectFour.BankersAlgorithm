092c86b703cd8aa26b112bf1eb88b5e544dc4a38
aPalermooo <ajp2s@login.missouristate.edu>
2024-10-17 22:15:09 -0500

    Deallocate a process

--- ProcessManager.py
+++ ProcessManager.py
@@ -14,11 +14,13 @@ from operator import truediv
 class ProcessManager:
     def __init__(self, numProcesses: int, numResources: int, allocated: list[list[int]], maxNeed: list[list[int]], current: list[int], request: list[int]):
 
+
         self.__numProcesses = numProcesses
         self.__numResources = numResources
         self.__allocated = allocated
         self.__maxNeed = maxNeed
-        self.__current = current
+        self.__current = []
+        self.__current.append(current)
         self.__request = request
 
         self.__currentNeed = []
@@ -46,7 +48,7 @@ class ProcessManager:
             print("Attempting process: " + str(process))
             allocatable = True
             for resource in range(self.__numResources):
-                if self.__currentNeed[process][resource] > self.__current[resource]:
+                if self.__currentNeed[process][resource] > self.__current[time][resource]:
                     allocatable = False
                     # print(f"{self.__currentNeed[process][resource]} !> {self.__current[resource]} = {allocatable}")
                     break
@@ -55,8 +57,20 @@ class ProcessManager:
                 break
             else:
                 print(f"{process} / {self.__numProcesses - 1} - Not Allocated")
+        print(f"\t\tProcess {processesWaiting[index]} can be allocated\n"
+              f"\t\tDeallocating Resources...")
+        # for resourceType, resourceAmount in enumerate(processesWaiting[index]):
+
+        print(f"\nAllocated Resources: {self.__allocated[index]}")
+        time += 1
+        # self.__current.append(self.__allocated[index])
+        self.__current.append([])
+        for resource in range(self.__numResources):
+            self.__current[time].append(self.__current[time-1][resource] + self.__allocated[index][resource])
         processesWaiting.pop(index)
-        print(processesWaiting)
+        print(f"Initial Allocation:  {self.__current[0]}")
+        print(f"Current Allocation:  {self.__current[time]}")
+        print(f"Queue:               {processesWaiting}")
 
 
 
@@ -82,7 +96,7 @@ class ProcessManager:
                 resourcesNeeded += str(self.__currentNeed[process][resource]) + " "
             needMatrix = needMatrix + f"{process}: {resourcesNeeded}" + "\n"
 
-        availablePre = " ".join(str(x) for x in self.__current)
+        availablePre = " ".join(str(x) for x in self.__current[0])
         return (f"There are {self.__numProcesses} process in the system.\n"
                 f"\n"
                 f"There are {self.__numResources} resource types.\n"
